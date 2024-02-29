7b7b315d28a8a46f0370bd0cf45ac28cea203084
Rahul Rai <rrai06125@gmail.com>
2024-02-29 22:39:21 +0530

    Crisp Chat system integrated!

--- src/App.js
+++ src/App.js
@@ -2,9 +2,15 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { About, Contact, Footer, Header, Home, Portfolio, Resume } from "./components";
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import { Crisp } from "crisp-sdk-web";
+import { useEffect } from "react";
 
 
 function App() {
+  useEffect(() => {
+    Crisp.configure("08d56c13-bdad-40c9-859d-c124c0b0b35a");
+  },[])
+
   return (
     <div>
       <BrowserRouter>
